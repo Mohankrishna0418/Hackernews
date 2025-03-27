5938befcb05ed545571435c34188f77085ee90cb
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-03-27 14:14:13 +0530

    add token middleware for JWT verification and update route index to include post routes

--- src/routes/route-index.ts
+++ src/routes/route-index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { authenticationRoutes } from "./authentication-routes";
 import { usersRoutes } from "./user-route";
+import { postRoutes } from "./post-route";
 
 export const allRoutes = new Hono();
 
 allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/user", usersRoutes); 
+allRoutes.route("/post", postRoutes);
