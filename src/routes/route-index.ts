d9645ef7c98bbc8276e8e7cb26fdfa1431155a2b
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-04-08 08:46:37 +0530

    feat: add Swagger UI integration and documentation for API endpoints

--- src/routes/route-index.ts
+++ src/routes/route-index.ts
@@ -4,9 +4,13 @@ import { usersRoutes } from "./user-route";
 import { postRoutes } from "./post-route";
 import { likeRoutes } from "./likes-route";
 import { commentsRoutes } from "./comments-route"
+import { swaggerUI } from "@hono/swagger-ui";
+import { swaggerDocument } from "./swagger-doc";
 
 export const allRoutes = new Hono();
 
+allRoutes.get("/ui", swaggerUI({ url: "/docs" }));
+allRoutes.route("/", swaggerDocument);
 allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/user", usersRoutes); 
 allRoutes.route("/post", postRoutes);
