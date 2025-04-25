11059ee45289ab113c7fde76242d0f6575808aaa
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-04-25 15:04:54 +0530

    integrate better-auth with Prisma for user authentication
    - Added Prisma schema for User, Post, Like, Comment, Session, Account, and Verification models.
    - Implemented better-auth server client with Prisma adapter for session management.
    - Created session middleware to handle authentication and session retrieval.
    - Configured CORS for API routes and added authentication routes.
    - Generated TypeScript definitions and JavaScript runtime for Prisma.

--- src/routes/route-index.ts
+++ src/routes/route-index.ts
@@ -6,11 +6,24 @@ import { likeRoutes } from "./likes-route";
 import { commentsRoutes } from "./comments-route"
 import { swaggerUI } from "@hono/swagger-ui";
 import { swaggerDocument } from "./swagger-doc";
+import { cors } from "hono/cors";
+import { authRoute } from "./middleware/session-middleware";
 
 export const allRoutes = new Hono();
 
+allRoutes.use(
+    cors({
+      origin: "http://localhost:4000",
+      allowHeaders: ["Content-Type", "Authorization"],
+      allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      credentials: true,
+    })
+  );
+  
+
 allRoutes.get("/ui", swaggerUI({ url: "/docs" }));
 allRoutes.route("/", swaggerDocument);
+allRoutes.route("/api/auth", authRoute)
 allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/user", usersRoutes); 
 allRoutes.route("/post", postRoutes);
