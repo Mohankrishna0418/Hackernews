8703e71ebfb11fda4762baf3fa8e77b2157b14b7
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-04-25 15:05:14 +0530

    fix: update environment variables and dependencies for better-auth integration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { serve } from '@hono/node-server'
-import { allRoutes } from './routes/route-index'
+import { serve } from "@hono/node-server";
+import { allRoutes } from "./routes/route-index";
 
 
-allRoutes.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+allRoutes.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
 serve(allRoutes, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  console.log(`Server is running on http://localhost:${info.port}`);
+});
